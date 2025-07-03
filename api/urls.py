c2ec3cd9a8e1ac20043eedc119a00335c72db1a8
Edwige08 <edwige.saves@gmail.com>
2025-07-03 15:45:41 +0200

    add: pet_statuses route

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import get_users, create_user, user_detail
+from .views import get_users, create_user, user_detail, get_pet_statuses, create_pet_statuses, pet_statuses_detail
 
 urlpatterns = [
     path('users/', get_users, name='get_users'),
     path('users/create/', create_user, name='create_user'),
-    path('users/<int:pk>', user_detail, name='user_detail')
+    path('users/<int:pk>', user_detail, name='user_detail'),
+
+    path('pet_statuses/', get_pet_statuses, name='get_pet_statuses'),
+    path('pet_statuses/create/', create_pet_statuses, name='create_pet_statuses'),
+    path('pet_statuses/<int:pk>', pet_statuses_detail, name='pet_statuses_detail')
 ]
--- api/views.py
+++ api/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Users
-from .serializer import UserSerializer
+from .models import Users, Pets_Statuses
+from .serializer import UserSerializer, PetStatusesSerializer
 
 @api_view(['GET'])
 def get_users(request):
@@ -41,4 +41,43 @@ def user_detail(request, pk):
     
     elif request.method == 'DELETE':
         user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
+def get_pet_statuses(request):
+    pet_statuses = Pets_Statuses.objects.all()
+    serializer = PetStatusesSerializer(pet_statuses, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_pet_statuses(request):
+    serializer = PetStatusesSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def pet_statuses_detail(request, pk):
+    try: 
+        pet_statuses = Pets_Statuses.objects.get(pk=pk)
+    except Pets_Statuses.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = PetStatusesSerializer(pet_statuses)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = PetStatusesSerializer(pet_statuses, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        pet_statuses.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)

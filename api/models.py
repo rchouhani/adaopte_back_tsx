56c8b2cfff478d8d1b68aca110bb4c1a2f6cebf5
rchouhani <rchouhani@me.com>
2025-07-03 14:54:09 +0200

    add : create models

--- api/models.py
+++ api/models.py
@@ -1,12 +1,94 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    age = models.IntegerField()
-    name = models.CharField(max_length=250)
-    # firstname = models.CharField(max_length=250)
-    # lastname = models.CharField(max_length=250)
+class Pets_Statuses(models.Model):
+    status_title = models.CharField(max_length=48)
+
+    def __str__(self):
+        return self.status_title
+    
+
+class Admins(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.username
+
+
+class Availabilities(models.Model):
+    frequency = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.frequency
+    
+
+class Users(models.Model):
+    birthdate = models.DateField(auto_now=False, auto_now_add=False)
+    firstname = models.CharField(max_length=250)
+    lastname = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+    email = models.EmailField(max_length=1024)
+    phone = models.CharField(max_length=250)
+    city = models.CharField(max_length=250)
+    zipcode = models.CharField(max_length=250)
+    motivation = models.CharField(max_length=1024)
+    volunteer = models.BooleanField(default=False)
+    adoptant = models.BooleanField(default=False)
+    availability_id = models.ForeignKey(Availabilities, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return self.name
+        return self.firstname, self.lastname, self.city, self.email
+    
+
+class Donations(models.Model):
+    amount_euros = models.IntegerField()
+    user_id = models.ForeignKey(Users, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.amount_euros
+    
+
+class Pets(models.Model):
+    pet_name = models.CharField(max_length=250)
+    status_id = models.ForeignKey(Pets_Statuses, on_delete=models.DO_NOTHING)
+    birthyear = models.DateField(auto_now=False, auto_now_add=False)
+    breed =  models.CharField(max_length=250)
+    pet_type =  models.CharField(max_length=250)
+    city =  models.CharField(max_length=250)
+    zipcode =  models.CharField(max_length=250)
+    pet_description = models.CharField(max_length=1024)
+    image_url = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.pet_name
+    
+
+class Petting_Dates(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.DO_NOTHING)
+    pet_id = models.ForeignKey(Pets, on_delete=models.DO_NOTHING)
+    appointment = models.DateTimeField(auto_now=False, auto_now_add=False)
+    duration_minutes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.appointment
+    
+
+class Adoptions(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.DO_NOTHING)
+    pet_id = models.ForeignKey(Pets, on_delete=models.DO_NOTHING)
+    adoptions_date = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.adoptions_date
---
+++ tables.sql
@@ -0,0 +1,120 @@
+-- Création des tables dans le bon ordre
+
+CREATE TABLE pets_statuses (
+    id serial primary key,
+    status_title varchar(255) NOT NULL
+);
+
+CREATE TABLE admins (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at DATE DEFAULT NOW() NOT NULL,
+    updated_at DATE
+);
+
+CREATE TABLE availabilities (
+    id SERIAL PRIMARY KEY,
+    frequency VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    birthdate DATE NULL,
+    password VARCHAR(255) NOT NULL,
+    e_mail VARCHAR(255) UNIQUE NOT NULL,
+    phone VARCHAR(20) NULL,
+    city VARCHAR(255) NOT NULL,
+    zipcode VARCHAR(20) NOT NULL,
+    motivation TEXT NOT NULL,
+    volunteer BOOLEAN DEFAULT FALSE NOT NULL,
+    adoptant BOOLEAN DEFAULT FALSE NOT NULL,
+    availability_id BIGINT NOT NULL,
+    created_at DATE DEFAULT NOW() NOT NULL,
+    updated_at DATE,
+    FOREIGN KEY (availability_id) REFERENCES availabilities(id)
+);
+
+CREATE TABLE donations (
+    id SERIAL PRIMARY KEY,
+    amount_euros BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    created_at DATE DEFAULT NOW() NOT NULL,
+    updated_at DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE pets(
+    id Serial Primary Key,
+    pet_name varchar(255) NOT NULL,
+    status_id bigint,
+    birthyear bigint,
+    breed varchar(255) NOT NULL,
+    pet_type varchar(255) NOT NULL,
+    city varchar(255) NOT NULL,
+    zipcode varchar(255) NOT NULL,
+    pet_description text NOT NULL,
+    image_url varchar(255) NOT NULL,
+    created_at DATE DEFAULT now() NOT NULL,
+    updated_at DATE,
+    FOREIGN KEY (status_id) REFERENCES pets_statuses(id)
+);
+
+CREATE TABLE petting_dates (
+    id serial primary key,
+    user_id bigint,
+    pet_id bigint,
+    appointment date,
+    duration_minutes bigint,
+    created_at DATE DEFAULT now() NOT NULL,
+    updated_at DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (pet_id) REFERENCES pets(id)
+);
+
+CREATE TABLE adoptions (
+    id serial primary key,
+    user_id bigint,
+    pet_id bigint,
+    adoption_date DATE DEFAULT now(),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (pet_id) REFERENCES pets(id)
+);
+
+
+-- SEEDING des tables 
+
+INSERT INTO pets_statuses (status_title) VALUES
+    ('available'),
+    ('adopted'),
+    ('dead');
+
+INSERT INTO admins (username, password) VALUES
+    ('admin', 'rofated');
+
+INSERT INTO availabilities (frequency) VALUES
+    ('Une fois par semaine'),
+    ('Une fois par mois'),
+    ('Une fois par trimestre'),
+    ('Une fois par semestre'),
+    ('Une fois par an');
+
+INSERT INTO users (firstname, lastname, password, e_mail, city, zipcode, motivation, volunteer, adoptant, availability_id) VALUES
+    ('Ada', 'Lovelace', 'adalovelace', 'ada@lovelace.com', 'Paris', 75010, 'J''ai un ami animal', true, false, 4),
+    ('Add', 'Option', 'addoption', 'add@option.com', 'Paris', 75010, 'J''ai été adoptée un jour', false, true, 4);
+
+INSERT INTO donations (amount_euros, user_id) VALUES
+    (2, 1);
+
+INSERT INTO pets (pet_name, status_id, birthyear, breed, pet_type, city, zipcode, pet_description, image_url) VALUES
+    ('Charlie', 1, 2020, 'Pug', 'Chien', 'Lille', '59000', 'Charlie est un petit chien calme et affectueux qui adore les balades tranquilles et les caresses sur le canapé.', '/images/charlesdeluvio-K4mSJ7kc0As-unsplash.jpg'),
+    ('Mia', 1, 2023, 'Chat noir et blanc', 'Chat', 'Strasbourg', '67000', 'Mia est une boule de tendresse curieuse et joueuse, toujours prête à ronronner près de vous.', '/images/manja-vitolic-gKXKBY-C-Dk-unsplash.jpg');
+
+INSERT INTO petting_dates (user_id, pet_id, appointment, duration_minutes) VALUES
+    (1, 1, now(), 60);
+
+INSERT INTO adoptions (pet_id, user_id, adoption_date) VALUES 
+    (2, 2, now());
+

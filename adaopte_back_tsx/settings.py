a92092c50428cab84c8fd0b1bc34cea2d5db388a
Edwige08 <edwige.saves@gmail.com>
2025-07-08 10:04:42 +0200

    update: settings.py (DEBUG)

--- adaopte_back_tsx/settings.py
+++ adaopte_back_tsx/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lr)g0uyoh7rqdt_0lpae+=&lad(mek&dh02(f2c++e+)s-_)%@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
